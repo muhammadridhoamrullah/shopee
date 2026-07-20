8f1c0f1a3ba03c131c8d03c0e9a12894fbad35fe
Muhammad Ridho Amrullah <ridhoamrullah99@gmail.com>
2026-07-20 14:36:57 +0700

    feat(nextjs-shopee-frontend-mainpage): main page kategori done, sudah menambahkan scroll, next flash sale

--- nextjs-shopee/src/components/MainPage/Kategori.tsx
+++ nextjs-shopee/src/components/MainPage/Kategori.tsx
@@ -1,8 +1,39 @@
+"use client";
+
 import { kategoriMenuLists } from "@/src/helpers/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef, useState } from "react";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import { toast } from "react-toastify";
 
 export default function Kategori() {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [showLeft, setShowLeft] = useState(false);
+  const [showRight, setShowRight] = useState(true);
+
+  const handleScroll = () => {
+    if (scrollRef.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+
+      setShowLeft(scrollLeft > 0);
+      setShowRight(scrollLeft + clientWidth < scrollWidth);
+    }
+  };
+
+  const handleLeftClick = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
+    }
+  };
+
+  const handleRightClick = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="bg-white w-full h-90  flex flex-col justify-between items-start">
       {/* Awal Text Kategori */}
@@ -14,10 +45,22 @@ export default function Kategori() {
       {/* Awal Menu Kategori */}
       <div className=" relative  flex-1 w-full">
         {/* Awal Panah Kiri */}
+        {showLeft && (
+          <button
+            onClick={handleLeftClick}
+            className="absolute left-0 top-1/2 -translate-y-1/2 z-10 cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out"
+          >
+            <IoIosArrowDropleftCircle className="w-10 h-10 text-[#EE4D2D]" />
+          </button>
+        )}
         {/* Akhir Panah Kiri */}
         {/* [&>*:not(:first-child)]:pl-2 [&>*:not(:last-child)]:pr-2 */}
         {/* Awal Mapping Menu Kategori */}
-        <div className="overflow-x-auto scroll-smooth flex-1 h-full grid grid-flow-col grid-rows-2 scrollbar-none divide-x-2 divide-[#F5F5F5]  ">
+        <div
+          onScroll={handleScroll}
+          ref={scrollRef}
+          className="overflow-x-auto scroll-smooth flex-1 h-full grid grid-flow-col grid-rows-2 scrollbar-none divide-x-2 divide-[#F5F5F5]  "
+        >
           {kategoriMenuLists.map((kategori, index) => (
             <Link
               href={kategori.link}
@@ -46,6 +89,14 @@ export default function Kategori() {
         {/* Akhir Mapping Menu Kategori */}
 
         {/* Awal Panah Kanan */}
+        {showRight && (
+          <button
+            onClick={handleRightClick}
+            className="absolute right-0 top-1/2 -translate-y-1/2 z-10 cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out"
+          >
+            <IoIosArrowDroprightCircle className="w-10 h-10 text-[#EE4D2D]" />
+          </button>
+        )}
         {/* Akhir Panah Kanan */}
       </div>
       {/* Akhir Menu Kategori */}
