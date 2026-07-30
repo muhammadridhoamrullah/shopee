cb3e17d69ad6cff962363340fc7382fbe5d7fba5
Muhammad Ridho Amrullah <ridhoamrullah99@gmail.com>
2026-07-30 22:22:23 +0700

    fix: lint at Banner

--- nextjs-shopee/src/components/MainPage/Banner/Banner.tsx
+++ nextjs-shopee/src/components/MainPage/Banner/Banner.tsx
@@ -53,7 +53,7 @@ export default function Banner() {
                 src={menu.image}
                 alt={menu.name}
                 fill
-                className="rounded-md object-cover"git s
+                className="rounded-md object-cover"
               />
             </div>
             {/* Akhir Image Menu */}
